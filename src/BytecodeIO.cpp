f80d2f0bce926c6e7756ee7205c5770d4e030d1c
Fred Nicolson <fred.nicolson@outlook.com>
2016-01-19 11:57:58 +0000

    Switched 'unsigned char' to 'unsigned int' throughout, and reworked how BytecodeIO reads/writes files. So numbers > 255 now work.

--- src/BytecodeIO.cpp
+++ src/BytecodeIO.cpp
@@ -10,39 +10,39 @@ BytecodeIO::~BytecodeIO()
     //dtor
 }
 
-bool BytecodeIO::readBytecode(const std::string& filepath, std::vector<unsigned int>& byteCode)
+bool BytecodeIO::readBytecode(const std::string& filepath, std::vector<unsigned int>& bytecode)
 {
-    std::streampos fileSize;
-    unsigned char * memblock;
-
+    //Open the file
     std::ifstream file;
     file.open(filepath, std::ios::in | std::ios::binary | std::ios::ate);
     if(!file.is_open())
         return false;
 
-    fileSize = file.tellg();
-    memblock = new unsigned char [fileSize];
+    //Get file size
+    std::streampos fileSize = file.tellg();
     file.seekg(0, std::ios::beg);
-    file.read((char*)memblock, fileSize);
+
+    //Reserve space in the vector and read the data
+    bytecode.resize(fileSize / sizeof(decltype(bytecode.begin())::value_type));
+    file.read((char*)&bytecode[0], fileSize);
+
     file.close();
-
-    for(unsigned int a = 0; a < fileSize; a++)
-        byteCode.emplace_back(static_cast<unsigned int>(memblock[a]));
-
-    delete[] memblock;
     return true;
 }
 
-bool BytecodeIO::writeBytecode(const std::string& filepath, const std::vector<unsigned int>& byteCode)
+bool BytecodeIO::writeBytecode(const std::string& filepath, const std::vector<unsigned int>& bytecode)
 {
     std::ofstream file;
     file.open(filepath, std::ios::out | std::ios::binary);
     if(!file.is_open())
         return false;
 
-    for(const auto &byte: byteCode)
-        file << byte;
+    file.write((char*)bytecode.data(), sizeof(decltype(bytecode.begin())::value_type) * bytecode.size());
 
+    std::cout << "\nBytecode: ";
+    for(const auto &byte: bytecode)
+        std::cout << (int)byte << ", ";
+    std::cout << std::endl;
     file.close();
     return true;
 }
